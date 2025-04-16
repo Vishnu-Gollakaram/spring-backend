eb44a53b2216f12c12bd9c961cbb67704066e7aa
vishnu-gollakaram <vishnugollakaram2021@gmail.com>
2025-04-16 06:30:24 +0530

    Adding CORS

--- src/main/java/com/vishnu/demo/HelloController.java
+++ src/main/java/com/vishnu/demo/HelloController.java
@@ -5,9 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class HelloController {
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/hello")
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello from Spring Boot!");
